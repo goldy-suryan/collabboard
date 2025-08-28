d10e230573c2d287a7af368ac071544183718f41
Goldy Suryan <goldy.suryan@builder.ai>
2025-08-28 23:48:00 +0530

    feat: added max and min pool size for DB connection

--- services/task-service/config/db.js
+++ services/task-service/config/db.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      maxPoolSize: 10,
+      minPoolSize: 2,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    });
     console.log('MONGODB connected');
   } catch (e) {
     console.log(e, 'error');
